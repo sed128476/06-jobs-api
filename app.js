bc536149e1097d35857b8f17a9a6c3b75af84340
sed128476 <afnasr09@gmail.com>
2025-01-05 21:37:40 -0800

    answer for week9

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
+//mongodb+srv://sarah:Sedna128@cluster0.cap6g.mongodb.net/06-JOBS-API?retryWrites=true&w=majority&appName=Cluster0
 require('dotenv').config();
 require('express-async-errors');
 const express = require('express');
 const app = express();
+
+// connectDB
+const connectDB = require('./db/connect')
+const authenticateUser = require('./middleware/authentication')
+// routers
+const authRouter = require('./routes/auth')
+const jobsRouter = require('./routes/jobs')
+
+
 
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
@@ -11,9 +21,10 @@ app.use(express.json());
 // extra packages
 
 // routes
-app.get('/', (req, res) => {
-  res.send('jobs api');
-});
+
+app.use('/api/v1/auth', authRouter)
+app.use('/api/v1/jobs',authenticateUser, jobsRouter)
+
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
@@ -22,6 +33,7 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URI)
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
